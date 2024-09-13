7c897dbe38260cfb4693bc5d64ce47b2cd37a4d9
loz-js <lozowo05@gmail.com>
2024-09-13 18:11:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-AudioParamMap.get ('/library',(req,res) => {
+app.get ('/library',(req,res) => {
     const songs = [
         { title: 'Start', artist: 'Asake', url:'https://www.youtube.com/watch?v=STKa-IIY6I8'},
         { title: 'Both Sides of a Smile', artist: 'Santan Dave', url: 'https://www.youtube.com/watch?v=STKa-IIY6I8'}
