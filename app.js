8005d748cb55a5ca46637e0763e47f463375bc34
loz-js <lozowo05@gmail.com>
2024-09-13 21:12:30 +0100

    commit 3

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+
+app.get(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index'); 
+});
+    
+app.get('/library', (req, res) => {  
+    const songs = [
+        { title: 'Start', artist: 'Asake', url:'https://www.youtube.com/watch?v=STKa-IIY6I8'},
+        { title: 'Both Sides of a Smile', artist: 'Santan Dave', url: 'https://www.youtube.com/watch?v=STKa-IIY6I8'}
+    ];
+    res.render('library', {songs});
+});
+
+const PORT = 3000;
+app.listen (PORT, () => {
+    console.log('Server is running on https://localhost:${PORT}');
+}); 
--- script.js
+++
@@ -1,7 +0,0 @@
-app.get ('/library',(req,res) => {
-    const songs = [
-        { title: 'Start', artist: 'Asake', url:'https://www.youtube.com/watch?v=STKa-IIY6I8'},
-        { title: 'Both Sides of a Smile', artist: 'Santan Dave', url: 'https://www.youtube.com/watch?v=STKa-IIY6I8'}
-    ];
-    res.render('library', {songs});
-});
